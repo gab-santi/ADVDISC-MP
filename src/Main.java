f4413566cb02899ff67ac19df27bd1cea3848df0
Luigi <luigi_acorda@dlsu.edu.ph>
2018-07-30 21:35:51 +0800

    Gauss-Jordan elimination/Reduced row-echelon form

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String args[]) {
+		
+		List<Vector> vectors = new ArrayList<Vector>();
+		vectors.add(new Vector(new double[] {-20,-10,10},3));
+		vectors.add(new Vector(new double[] {0,10,-5},3));
+		vectors.add(new Vector(new double[] {-10,10,15},3));
+		
+		Vector.Gauss_Jordan(vectors, 3, new Vector(3));
+	}
+}
--- src/Vector.java
+++ src/Vector.java
@@ -52,21 +52,88 @@ public class Vector {
 		return this;
 	}
 	
-	public boolean isSolvable (List<Vector> vectors, Vector constants) {
+	public static void swap (List<Vector> vectors, Vector constants, int i, int j) {
+		Vector vector = vectors.get(i);
+		vectors.set(i, vectors.get(j));
+		vectors.set(j, vector);
+		
+		double constant = constants.getVector()[i];
+		constants.getVector()[i] = constants.getVector()[j];
+		constants.getVector()[j] = constant;
+	}
+	
+	public static boolean isSolvable (List<Vector> vectors, Vector constants) {
 		if (vectors.size() == constants.getVector().length)
 			return true;
 		else
 			return false;
 	}
 	
-	public Vector Gauss_Jordan (List<Vector> vectors, int dimension, Vector constants) {
-		Vector v = new Vector (dimension);
-		
+	public static Vector Gauss_Jordan (List<Vector> vectors, int dimension, Vector constants) {
 		if (isSolvable(vectors, constants)) {
-			// to-do: perform Gauss-Jordan algorithm
+			//4 return to step 1
+			for (int i = 0; i < vectors.size(); i++) {
+				
+				/*DELETE THIS BEFORE SUBMISSION*/
+				for(int counter = 0; counter < vectors.size(); counter++) {
+					String vector = "";
+					for(double d : vectors.get(counter).getVector())
+						vector += d + " ";
+					System.out.println(vector.trim() + " " + constants.getVector()[counter]);
+				}
+				System.out.println("");
+				/*DELETE THIS BEFORE SUBMISSION*/
+				
+				if (vectors.get(i).getVector().length != dimension)
+					return null;
+				
+				int j = 0, k = i;
+				double pivot = 0;
+				
+				//1 if matrix[i][j] == 0 swap some row below until matrix[i][j] != 0
+				while (vectors.get(i).getVector()[j] == 0) {
+					if(k == vectors.size())
+						return null;
+					
+					if (j == vectors.get(i).getVector().length - 1) {
+						swap(vectors, constants, i, k);
+						j = 0;
+						k++;
+					}
+					else
+						j++;
+				}
+				
+				//2 Divide the ith row by matrix[i][j] to make pivot value = 1
+				pivot = vectors.get(i).getVector()[j];
+				vectors.get(i).scale(1 / pivot);
+				constants.getVector()[i] *= 1 / pivot;
+				
+				//3 Make values in jth column 0 by using elementary row operations
+				for (int l = 0; l < vectors.size(); l++) {
+					if (l != i && vectors.get(l).getVector()[l] != 0) {
+						Vector scaledVector = new Vector(vectors.get(i).getVector(), vectors.get(i).getDimension());
+						double scalar = vectors.get(l).getVector()[j] * -1;
+						
+						scaledVector.scale(scalar);
+						vectors.get(l).add(scaledVector);
+						
+						constants.getVector()[l] += constants.getVector()[i] * scalar;
+					}	
+				}
+			}
 			
+			/*DELETE THIS BEFORE SUBMISSION*/
+			for(int counter = 0; counter < vectors.size(); counter++) {
+				String vector = "";
+				for(double d : vectors.get(counter).getVector())
+					vector += d + " ";
+				System.out.println(vector.trim() + " " + constants.getVector()[counter]);
+			}
+			System.out.println("");
+			/*DELETE THIS BEFORE SUBMISSION*/
 			
-			return v;
+			return constants;
 		} else {
 			return null;
 		}
@@ -93,5 +160,9 @@ public class Vector {
 	
 	public double[] getVector () {
 		return vector;
+	}
+	
+	public int getDimension () {
+		return dimension;
 	}
 }
