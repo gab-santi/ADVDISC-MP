0a065d263799d64136190da054c2a36268c986e3
gab-santi <gabbysantiago96@yahoo.com>
2018-08-06 22:08:51 +0800

    Added Matrix template

---
+++ src/Matrix.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+/*
+ * ACORDA, Luigi
+ * GO, Timothy
+ * SANTIAGO, Gabriel
+ * 
+ * ADVDISC S17
+ */
+
+public class Matrix {
+	
+	public Matrix (int dimension) {
+		
+	}
+	
+	public Matrix (List<Vector> list, int dimension) {
+		
+	}
+	
+	public Matrix times (Matrix other) {
+		Matrix product = new Matrix(0);
+		
+		return product;
+	}
+	
+	public double det () {
+		
+		return 0.0;
+	}
+	
+	public Matrix inverse () {
+		
+		return null;
+	}
+	
+
+}
